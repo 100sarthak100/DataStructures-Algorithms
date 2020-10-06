6e050c2491e6aba787205da29d5f6107f30946fc
Abhi-thecoder <abhishek999.ad@gmail.com>
2020-10-06 18:25:48 +0530

    Requested Changes Made

--- Code/LeetcodePattern/BFS/BinaryTreeRightSideView.cpp
+++ Code/LeetcodePattern/BFS/BinaryTreeRightSideView.cpp
@@ -1,3 +1,22 @@
+/*Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example:
+
+Input: [1,2,3,null,5,null,4]
+Output: [1, 3, 4]
+Explanation:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+Time - O(N)
+Space - O(1)
+
+*/
+
 class Solution {
 public:
     
