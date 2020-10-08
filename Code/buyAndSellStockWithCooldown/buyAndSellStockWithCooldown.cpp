df968a15a6067a81b79f62f6ac6d53d73d89429e
Andrew Jyan <andrew.jyan4@gmail.com>
2020-10-08 08:07:32 -0400

    added comments for buyandsellstock

--- Code/buyAndSellStockWithCooldown/buyAndSellStockWithCooldown.cpp
+++ Code/buyAndSellStockWithCooldown/buyAndSellStockWithCooldown.cpp
@@ -1,3 +1,16 @@
+// Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) 
+// with the following restrictions:
+
+// You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+// After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+
+// Input: [1,2,3,0,2]
+// Output: 3 
+// Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+// Time - O(N)
+// Space - O(1)
+
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
